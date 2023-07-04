9aa8ba287692bb9bdb6fa77f3dee9ce8e9b12aa4
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2023-07-04 06:09:32 -0400

    improved install.sh
    Not only should be more cross-distro compatible now, but does mass theme import from current dir

--- install.sh
+++ install.sh
@@ -1,5 +1,37 @@
-#!/bin/sh
-set -v
+#!/bin/bash
+##################################################################################
+# v0.1
+# created by K0K0$H@
+# purpose: add .xml color themes in current dir to gedit
+# Github repo: https://github.com/K0K0SHA/gedit-themes/
+# repo cloned from: https://github.com/mig/gedit-themes/
+# this install.sh works with both repos above
+# be sure to check readme
 
-mkdir -p $HOME/.gnome2/gedit/styles
-cp ./*.xml $HOME/.gnome2/gedit/styles/.
+styles_dir="$HOME/.local/share/gedit/styles"
+
+if [ ! -d "$styles_dir" ]; then
+  echo "The directory $styles_dir does not exist."
+  echo "Check gedit config file, as you may need to edit it"
+  # TODO attempt gedit config edit if user wants
+  exit 1
+fi
+
+# ensure there is at least one theme in current directory
+shopt -s nullglob
+
+xml_files=(*.xml)
+
+if [ ${#xml_files[@]} -eq 0 ]; then
+  echo "No XML files found in the current directory."
+  echo "Ensure you are in the correct directory with style xml files."
+  exit 1
+fi
+
+# functional one liner (most important line of code)
+# copies themes
+cp ./*.xml $styles_dir
+
+echo "Themes copied to $styles_dir"
+echo "Contents of $styles_dir (user-installed themes):"
+ls $styles_dir
